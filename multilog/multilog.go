f0409c0d088e114fa7ec640ef33e4152990a1c41
Brandon Powell <bpowell29a@gmail.com>
2013-03-19 09:52:56 -0400

    Added error to multilog.

--- multilog/multilog.go
+++ multilog/multilog.go
@@ -6,15 +6,15 @@ import (
 	"io"
 )
 
-func NewMultiLog(prefix string, flag int, filename string) *log.Logger{
+func NewMultiLog(prefix string, flag int, filename string) (mlog *log.Logger, err error){
 	//Open up log file for writing/appending
 	logf, err := os.OpenFile(filename, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0640)
 	if err != nil {
-		log.Fatalln(err)
+        return nil, err
 	}
 
 	//Setup the new log to use a MultiWriter to write to log file and stdout
-	mlog := log.New(io.MultiWriter(logf, os.Stdout), prefix, flag)
+	mlog = log.New(io.MultiWriter(logf, os.Stdout), prefix, flag)
 
-	return mlog
+	return mlog, err
 }
--- multilog/multilog_test.go
+++ multilog/multilog_test.go
@@ -7,7 +7,7 @@ import (
 func TestNewMultiLog(t *testing.T) {
 	defer os.Remove("testing.log")
 
-	a = multilog.NewMultiLog("Logging", 6, "testing.log")
+	a, _ = multilog.NewMultiLog("Logging", 6, "testing.log")
 
 	a.Println("Hello from TestNewMultiLog")
 }
